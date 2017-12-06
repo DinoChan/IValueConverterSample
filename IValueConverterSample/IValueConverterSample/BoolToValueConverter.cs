50e451e16b1dbd25eaa3fbbee2d6e241f4488a0c
Dino Chan <dino.chan@live.com>
2017-12-06 16:31:16 +0800

    Add Project.

---
+++ IValueConverterSample/IValueConverterSample/BoolToValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace IValueConverterSample
+{
+    public class BoolToValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null || (bool)value == false)
+                return DependencyProperty.UnsetValue;
+            else
+                return parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return object.Equals(value, parameter);
+        }
+    }
+}
---
+++ IValueConverterSample/IValueConverterSample/BoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace IValueConverterSample
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public bool InvertBoolean { get; set; } = false;
+
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var result = Visibility.Collapsed;
+
+            if ((value as bool? ^ InvertBoolean) == true)
+                result = Visibility.Visible;
+            
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var visibility = (Visibility)value;
+            return (visibility == Visibility.Visible) ^ InvertBoolean;
+        }
+    }
+}
